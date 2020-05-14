251c13663c2c09a33c5d83426466ab23a1efe71c
Misqual Zarabi <50719263+misqualzarabi@users.noreply.github.com>
2020-05-14 03:45:16 +0100

    changed package version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="my_lambdata_zarabi",  # the name that you will install via pip
-    version="1.2",
+    version="1.3",
     author="Misqual Zarabi",
     author_email="misqualzarabi234@yahoo.com",
     description="A short description",
